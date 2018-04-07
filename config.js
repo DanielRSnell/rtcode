9d4fd5a150d2b6f56af8932ee99c4ceb906a2ccd
DanielRSnell <hi@danielsnell.ninja>
2018-04-07 17:51:05 -0400

    first push

---
+++ config.js
@@ -0,0 +1,13 @@
+'use strict'
+
+module.exports = {
+  mailer: {
+    service: 'Gmail',
+    auth: {
+      user: 'dev@danielsnell.ninja',
+      pass: 'dev1'
+    }
+  },
+  dbConnstring: `mongodb://sample:sample@ds237989.mlab.com:37989/code4share`,
+  sessionKey: 'HaloCode4Share'
+}
